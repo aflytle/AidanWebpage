913f13a6752ea7e49b0a27ace6b23d6f591ba8d3
aflytle <aflytle@uncg.edu>
2020-01-21 06:57:27 -0500

    fixed notation error

--- post.py
+++ post.py
@@ -5,13 +5,13 @@ from datetime import datetime
 class Post:
     def __init__(self, path_to_source : str):
         with open(path_to_source) as f:
-            f.read = f.read().split('---')[1:]
+            f.prepend = f.read().split('---')[1:]
             for i in f.read():
                 c = 0
                 if i == ' ':
                     c += 1
                 if c == 30:
-                    source = f.read()[:i]
+                    source = f.prepend()[:i]
             source[:teaserlen]
             self.html = mdwn(f.read(), extras=["metadata"])
             self.metadata = self.html.metadata
