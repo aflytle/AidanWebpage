25d1cc30c688f68fb54067ce249de234ec572b05
Ellie Mae Galyean <elliemaegalyean@gmail.com>
2020-01-21 12:34:30 -0500

    fixed logic

--- post.py
+++ post.py
@@ -5,13 +5,19 @@ import os
 class Post:
     def __init__(self, path_to_source : str):
         with open(path_to_source) as f:
-            prepend = f.read().split('---')[1:]
-            for i in f.read():
-                c = 0
-                if i == ' ':
-                    c += 1
-                if c == 30:
-                    source = prepend()[:i]
+            source = "---".join(f.read().split('---')[1:])
+            #for i in prepend.split():
+            #    c = 0
+            #    if i == ' ':
+            #        c += 1
+            #    if c == 30:
+            #        self.source = prepend()[:i]
+            teaser = ""
+            for i in range(len(source)):
+                if i >= 150 and source[i] == " ":
+                    break
+                teaser += source[i]
+
             source[:teaserlen]
             self.html = mdwn(f.read(), extras=["metadata"])
             self.metadata = self.html.metadata
